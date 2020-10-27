fb5a9f07fa7b81b6ab8cbd8c85e9b39a618b1488
Jessica Pena <jcpena3775@gmail.com>
2020-10-27 14:55:10 -0500

    hwk update

--- UFO-Level1/StarterCode/static/js/app.js
+++ UFO-Level1/StarterCode/static/js/app.js
@@ -1,4 +1,70 @@
 // from data.js
-var tableData = data;
+const tableData = data;
+var tbody = d3.select("tbody");
+function intTable(data){
+    tbody.html("");
 
+    data.forEach((ufoData) =>  {
+    var datarow =  tbody.append("tr");
+    // loop through each field in the data
+    Object.values(ufoData).forEach((value)=>{
+    var datacell = datarow.append("td")
+        datacell.text(value);
+     } )
+     })
+};
+intTable(tableData);
 // YOUR CODE HERE!
+//select <tbody>
+// var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+// var table = d3.select("table");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter(){
+    // let filterData = tableData;
+    var input = d3.select("input");
+    var inputValue = input.property("value");
+// return value if matches
+var filterDate = data.filter(data => data.datetime === inputValue)
+// filterDate.forEach((runfilterData) => { 
+//     var datarow =  tbody.append("tr");
+//     // loop through each field in the data
+//     Object.values(runfilterData).forEach((value) => {
+//     var datacell = datarow.append("td")
+//         datacell.text(value);
+    //  } )
+    intTable(filterDate);
+
+};
+
+
+
+
+// function intTable(data){
+//     tbody.html("");
+
+//     data.forEach((ufoData) =>  {
+//     var datarow =  tbody.append("tr");
+//     // loop through each field in the data
+//     Object.values(ufoData).forEach((value)=>{
+//     var datacell = datarow.append("td")
+//         datacell.text(value);
+//      } )
+//      })
+// };
+// intTable(tableData);
+
+
+
+
+
+
+
+
+
+
+
